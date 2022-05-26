625710d0b011550afac41dbcf489a59e48c330a7
thimmdmev <thiagomm.dev@gmail.com>
2022-05-26 20:00:51 -0300

    :tada: New Project
    Create CardGame Component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+const CardGame = ( ) => {
+    return `
+    <article class="card-game">
+        <img src="/images/alura-pixel.png">
+    </article>
+    `
+};
+
+export default CardGame;
